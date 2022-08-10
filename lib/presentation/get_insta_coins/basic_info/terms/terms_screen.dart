3e7642e119b45f905b3ad2f0b76d0ac10f463e07
Jinwoo <njw9108@instapay.kr>
2022-08-10 11:24:43 +0900

    약관 화면 수정

--- lib/presentation/get_insta_coins/basic_info/terms/terms_screen.dart
+++ lib/presentation/get_insta_coins/basic_info/terms/terms_screen.dart
@@ -81,17 +81,19 @@ class _TermsScreenState extends State<TermsScreen> {
                     Center(
                       child: Text(
                         'INC 발행 및 구매관련 약관',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.w700,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                     Center(
                       child: Text(
                         '(Simple Agreement of Future Token)',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 30,
+                          fontSize: 28,
                         ),
                       ),
                     ),
